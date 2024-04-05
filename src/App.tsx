1107b56a9094434d2a58a50d7ece09cb923be00f
David Linhardt <76444188+MysterionAutotronic@users.noreply.github.com>
2024-04-05 15:27:53 +0200

    configure react to use typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Login from './pages/login/login';
+import Login from './pages/Login/Login';
 import Admin from './pages/Admin/Admin';
 import Invite from './pages/Invite/Invite';
 import Commit from './pages/Commit/Commit';
