4743f4f40c820c48a818bded0a54291e368509c3
David Linhardt <76444188+MysterionAutotronic@users.noreply.github.com>
2024-04-08 16:01:54 +0200

    fix boolean cell renderer

--- src/pages/Admin/components/Users.tsx
+++ src/pages/Admin/components/Users.tsx
@@ -30,7 +30,6 @@ export default function Users() {
             filter: true,
             sortable: true,
             editable: false,
-            cellRenderer: 'booleanCellRenderer',
             cellRendererParams: { disabled: true },
         },
         {
