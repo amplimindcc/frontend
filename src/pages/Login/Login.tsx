a385e38a8d1e772a48f6a461de04d28d9ecbd11c
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2024-04-09 13:37:35 +0200

    init html structure

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,22 @@
+import { useState } from 'react';
 import './Login.css'
 
 export default function Login() {
-    return (<></>);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <div>
+            <form className="login-form">
+                <div className="input-wrapper">
+                    <label htmlFor="username">username:</label>
+                    <input name="username"></input>
+                </div>
+                <div className="input-wrapper">
+                    <label htmlFor="username">password:</label>
+                    <input name="password"></input>
+                </div>
+            </form>
+        </div>
+    );
 }
