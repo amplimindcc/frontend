5a1555cd84805daa2906d2956915d0ddd8e371e8
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2024-04-09 13:52:55 +0200

    map values

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,22 +1,39 @@
 import { useState } from 'react';
 import './Login.css'
 
-export default function Login() {
+const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
+    const mapUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(e.target.value);
+    };
+
+    const mapPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log('username:', username);
+        console.log('password:', password); // remove later
+    };
+
     return (
         <div>
-            <form className="login-form">
+            <form onSubmit={handleSubmit} className="login-form">
                 <div className="input-wrapper">
                     <label htmlFor="username">username:</label>
-                    <input name="username"></input>
+                    <input type="text" name="username" value={username} onChange={mapUsername}></input>
                 </div>
                 <div className="input-wrapper">
                     <label htmlFor="username">password:</label>
-                    <input name="password"></input>
+                    <input type="password" name="password" value={password} onChange={mapPassword}></input>
                 </div>
+                <button type="submit" className="login-button">login</button>
             </form>
         </div>
     );
-}
+};
+
+export default Login;
