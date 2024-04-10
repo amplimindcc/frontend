22113e973220f2300e7074eb0cc3e15debecc073
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2024-04-10 16:06:35 +0200

    invite page: finish token sketch

--- src/pages/Invite/Invite.tsx
+++ src/pages/Invite/Invite.tsx
@@ -1,6 +1,6 @@
 import './Invite.css'
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import Register from './components/Register';
 import * as jose from 'jose';
 
@@ -8,14 +8,18 @@ const Invite = () => {
     const [token, setToken] = useState('');
     const [date, setDate] = useState('');
     const [dateValid, setDateValid] = useState(false);
+    const [registered, setRegistered] = useState(false);
+    const [loggedIn, setLoggedIn] = useState(false);
 
+    const navigate = useNavigate();
     //const params = useParams();
     //const { token } = params;
 
+    // all token related functions will be redone later
     const generateFakeToken = async (date: string) => {
         const fakeToken = await new jose.UnsecuredJWT({ date })
             .encode();
-            
+
         setToken(fakeToken);
         return fakeToken;
     };
@@ -39,10 +43,32 @@ const Invite = () => {
         setDateValid(dateFromToken > currentDate);
 
         return dateFromToken > currentDate;
-    }
+    };
+
+    const checkRegistered = async () => {
+        // check with call to backend if user password is already set
+    };
+
+    const init = async () => {
+        const valid = await checkDate();
+
+        if(valid) {
+            // if date is valid, check if user is already registered
+            // if user is not registered, display registration form
+            // if user is registered, check if user is logged in
+            // if user is not logged in, display login form
+            if(registered && !loggedIn) {
+                navigate("/login");
+            }
+        }
+        else {
+            // if date is not valid, display error message
+        }
+    };
 
     useEffect(() => {
-        checkDate();
+        init();
+
     }, []);
 
     return (
@@ -53,7 +79,7 @@ const Invite = () => {
             <br/>
             Date validity: {dateValid ? 'Valid' : 'Invalid'}
             <br/>
-            <Register display={dateValid}/>
+            <Register display={dateValid && !registered}/>
         </div>
     );
 };
