369c9026a2e9767122e86c8ea3d59c3b5335f20d
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2024-04-10 10:22:10 +0200

    invite page: generate fake token with date

--- src/pages/Invite/Invite.tsx
+++ src/pages/Invite/Invite.tsx
@@ -1,10 +1,32 @@
 import './Invite.css'
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Register from './components/Register';
+import * as jose from 'jose';
 
 const Invite = () => {
-    const params = useParams();
-    const { token } = params;
+    const [token, setToken] = useState('');
+
+    //const params = useParams();
+    //const { token } = params;
+
+    const secretKey = new TextEncoder().encode(
+        'secretKey',
+    );
+
+    const generateFakeToken = async (payload: Date) => {
+        const fakeToken = await new jose.SignJWT({ payload })
+            .setProtectedHeader({ alg: 'HS256' })
+            .sign(secretKey);
+        setToken(fakeToken);
+    };
+
+    useEffect(() => {
+        const date = new Date();
+        date.setDate(date.getDate() + 3);
+
+        generateFakeToken(date);
+    }, []);
 
     return (
         <div>
