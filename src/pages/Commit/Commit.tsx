88c876ca612bb6ddda4ad9073703326b2429abe0
Matthias Roy <impykid@hotmail.de>
2024-04-10 19:36:17 +0200

    added simple chat system

--- src/pages/Commit/Commit.tsx
+++ src/pages/Commit/Commit.tsx
@@ -1,17 +1,26 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import './Commit.css'
 
 const Commit = () => {
     const introText = "Das ist ein Beispielintro-Text";
     const exerciseText = "";
-    const optionalChat = "None";
+    const [optionalChat, setOptionalChat] = useState<string>('');
 
     const filePath = useRef(null);
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmitFileUpload = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         console.log('filePath:', filePath); // remove later
     };
+
+    const handleSubmitChat = (e: React.FormEvent<HTMLFormElement>) => {
+       e.preventDefault();
+       var message = e.target.chatMessage.value;
+       console.log('Sent message: ', message); // remove later
+       if (message != "")
+        setOptionalChat(optionalChat + (optionalChat != "" ? "\r\n" : "") + message);
+       e.target.chatMessage.value = "";
+    }
 
     return (
         <div>
@@ -22,10 +31,14 @@ const Commit = () => {
             <p>{exerciseText}</p>
             <br />
             <h3>Optional Chat:</h3>
-            <p>{optionalChat}</p>
+            <textarea value={optionalChat} rows={4} cols={40} readOnly={true} />
+            <form onSubmit={handleSubmitChat}>
+                <input type='text' name='chatMessage' />
+                <button type="submit">Send Message</button>
+            </form>
             <br />
             <h4>Upload your exercise:</h4>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmitFileUpload}>
                 <input type='file' ref={filePath} />
                 <button type="submit">Upload</button>
             </form>
