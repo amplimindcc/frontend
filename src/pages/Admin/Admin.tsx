751c5910d02f0fb1562fdf4271d8504a5c15919a
Timo Hauser <121182775+Edgarianom1212@users.noreply.github.com>
2024-04-15 19:35:34 +0200

    added Admin Submission Management

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -1,6 +1,8 @@
 import './Admin.css'
 import Users from './components/Users/Users'
+import Submissions from './components/Submissions/Submissions'
 
 export default function Admin() {
-    return (<Users/>);
+
+    return (<Submissions/>);
 }
