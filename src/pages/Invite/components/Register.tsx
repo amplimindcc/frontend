03b0530cdfb51a1dacc947059ebfe55ad650111d
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2024-04-17 17:44:11 +0200

    invite page: add validation

--- src/pages/Invite/components/Register.tsx
+++ src/pages/Invite/components/Register.tsx
@@ -1,43 +1,110 @@
 import './Register.css'
 import { useState } from 'react';
+import Error from '../../../components/Error/Error';
 
 interface RegisterProps {
     display: boolean;
 }
 
 const Register = ({ display }: RegisterProps) => {
-    if(display === null) {
+    if (display === null) {
         return null;
     }
 
-    const [password, setPassword] = useState('');
-    const [passwordRepeat, setPasswordRepeat] = useState('');
+    const [inputValues, setInputValues] = useState({
+        password: '',
+        passwordRepeat: ''
+    });
 
-    const mapPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(e.target.value);
+    const [errors, setErrors] = useState({
+        password: {
+            text: '',
+            valid: false
+        },
+        passwordRepeat: {
+            text: '',
+            valid: false
+        }
+    });
+    const [valid, setValid] = useState(false);
+
+    const validateInputValues = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newError = { ...errors };
+
+        if(e.target.name === 'password') {
+            if(e.target.value.length < 8) {
+                newError.password.text = 'Password must be at least 8 characters long';
+                newError.password.valid = false;
+            } else {
+                newError.password.text = '';
+                newError.password.valid = true;
+            }
+        }
+
+        if(e.target.name === 'passwordRepeat') {
+            if(e.target.value !== inputValues.password) {
+                newError.passwordRepeat.text = 'Passwords do not match';
+                newError.passwordRepeat.valid = false;
+            } else {
+                newError.passwordRepeat.text = '';
+                newError.passwordRepeat.valid = true;
+            }
+        }
+
+        setErrors(newError);
+
+        if(newError.password.valid && newError.passwordRepeat.valid) {
+            setValid(true);
+        }
+        else {
+            setValid(false);
+        }
     };
 
-    const mapPasswordRepeat = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setPasswordRepeat(e.target.value);
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        validateInputValues(e);
+
+        setInputValues({
+            ...inputValues,
+            [e.target.name]: e.target.value
+        });
     };
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log('submit');
+        if(valid) {
+            console.log('submit');
+        }
     };
 
     return (
         <div>
             <form className="register-form" onSubmit={handleSubmit}>
                 <div className="input-wrapper">
-                    <label htmlFor="password">password:</label>
-                    <input type="password" name="password"></input>
+                    <div className="input-label">
+                        <label htmlFor="password">password:</label>
+                        <input
+                            type="password"
+                            name="password"
+                            value={inputValues.password}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <Error text={errors.password.text} />
                 </div>
                 <div className="input-wrapper">
-                    <label htmlFor="password-repeat">password repeat:</label>
-                    <input type="password" name="password-repeat"></input>
+                    <div className="input-label">
+                        <label htmlFor="password-repeat">password repeat:</label>
+                        <input
+                            type="password"
+                            name="passwordRepeat"
+                            value={inputValues.passwordRepeat}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <Error text={errors.passwordRepeat.text} />
                 </div>
-                <button type="submit" className="register-button">set password</button>
+                <button type="submit" className="register-button" disabled={!valid}>set password</button>
             </form>
         </div>
     );
