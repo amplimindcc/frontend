c012f5e281d2028d6b8b3d26b9bb155076de24db
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2024-04-22 19:48:28 +0200

    login page: use toast

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,23 +2,28 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import user from '../../services/user';
 import './Login.css';
-import Notification from '../../components/Notification/Notification';
+import { ToastType } from '../../interfaces/ToastType';
+import toast from '../../services/toast';
 
 const Login = () => {
     const [inputValues, setInputValues] = useState({
         email: '',
         password: '',
     });
-    const [error, setError] = useState<string | null>(null);
 
     const navigate = useNavigate();
 
     useEffect(() => {
         const checkLogin = async () => {
-            const res = await user.authenticated();
+            try {
+                const res = await user.authenticated();
 
-            if(res.ok) {
-                navigate('/commit');
+                if(res.ok) {
+                    navigate('/commit');
+                }
+            }
+            catch(err) {
+                toast.showToast(ToastType.ERROR, 'Server authentication error. Try again later.');
             }
         };
         checkLogin();
@@ -33,34 +38,27 @@ const Login = () => {
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        let message = '';
 
         try {
             const res = await user.login(inputValues.email, inputValues.password);
 
             if(!res.ok) {
-                message = 'Invalid email or password';
+                toast.showToast(ToastType.ERROR, toast.httpError(res.status, 'Invalid email or password'));
             }
             else {
-                message = 'Login successful. Redirecting...';
+                toast.showToast(ToastType.INFO, 'login successful');
                 setTimeout(() => {
                     navigate('/commit');
                 }, 2000);
             }
         }
         catch(err) {
-            message = 'Error while logging in. Try again later.';
+            toast.showToast(ToastType.ERROR, 'Error while logging in. Try again later.');
         }
-
-        setError(message);
-        setTimeout(() => {
-            setError(null);
-        }, 3000);
     };
 
     return (
         <div className="center">
-            <Notification text={error} timeout={3000}/>
             <form className="login-form" onSubmit={handleSubmit}>
                 <div className="input-with-label">
                     <label htmlFor="email">email:</label>
