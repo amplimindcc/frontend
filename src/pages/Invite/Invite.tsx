2f2adb7bf50dda9dd4d0f2e210c1747cd211385d
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2024-04-24 11:30:37 +0200

    cleanup

--- src/pages/Invite/Invite.tsx
+++ src/pages/Invite/Invite.tsx
@@ -1,7 +1,6 @@
 import './Invite.css';
 import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import * as jose from 'jose';
 import user from '../../services/user';
 
 const Invite = () => {
@@ -15,48 +14,11 @@ const Invite = () => {
     //const params = useParams();
     //const { token } = params;
 
-    // all token related functions will be redone later
-    const generateFakeToken = async (date: string) => {
-        const fakeToken = await new jose.UnsecuredJWT({ date }).encode();
-
-        setToken(fakeToken);
-        return fakeToken;
-    };
-
-    const generateAndDecryptFakeToken = async () => {
-        const date = new Date();
-        date.setDate(date.getDate() + 3);
-
-        const fakeToken = await generateFakeToken(date.toDateString());
-        const { payload } = await jose.UnsecuredJWT.decode(fakeToken);
-        const dateFromToken = payload.date as string;
-        -setDate(dateFromToken);
-
-        return dateFromToken;
-    };
 
     const checkDate = async () => {
-        const dateFromToken = new Date(await generateAndDecryptFakeToken());
-        const currentDate = new Date();
-        setDateValid(dateFromToken > currentDate);
-
-        return dateFromToken > currentDate;
     };
 
     const init = async () => {
-        const valid = await checkDate();
-
-        if (valid) {
-            // if date is valid, check if user is already registered
-            // if user is not registered, display registration form
-            // if user is registered, check if user is logged in
-            // if user is not logged in, display login form
-            if (registered && !loggedIn) {
-                navigate('/login');
-            }
-        } else {
-            // if date is not valid, display error message
-        }
     };
 
     useEffect(() => {
