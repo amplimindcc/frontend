fba9d28238210ca233d0f29c213639bf9c25aa5c
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2024-04-25 14:23:08 +0200

    add invite request

--- src/pages/Invite/Invite.tsx
+++ src/pages/Invite/Invite.tsx
@@ -1,20 +1,39 @@
 import './Invite.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import user from '../../services/user';
-import Register from './components/Register';
+import Error from '../../components/Error/Error';
 import serviceHelper from '../../services/serviceHelper';
 import { ToastType } from '../../interfaces/ToastType';
 import toast from '../../services/toast';
+import Loader from '../../components/Loader/Loader';
 
 const Invite = () => {
     const navigate = useNavigate();
     const params = useParams();
     const { token } = params;
+    const [authenticated, setAuthenticated] = useState<Boolean | null>(null);
 
     if(token === null) {
         navigate('/login');
     }
+
+    const [inputValues, setInputValues] = useState({
+        password: '',
+        passwordRepeat: '',
+    });
+
+    const [errors, setErrors] = useState({
+        password: {
+            text: '',
+            valid: false,
+        },
+        passwordRepeat: {
+            text: '',
+            valid: false,
+        },
+    });
+    const [valid, setValid] = useState(false);
 
     useEffect(() => {
         const checkLogin = async () => {
@@ -23,6 +42,10 @@ const Invite = () => {
 
                 if(res.ok) {
                     await serviceHelper.routeBasedOnRole(navigate);
+                    setAuthenticated(true);
+                }
+                else {
+                    setAuthenticated(false);
                 }
             }
             catch(err) {
@@ -32,9 +55,114 @@ const Invite = () => {
         checkLogin();
     }, []);
 
+    const validateInputValues = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newError = { ...errors };
+
+        if (e.target.name === 'password') {
+            if (e.target.value.length < 8) {
+                newError.password.text =
+                    'Password must be at least 8 characters long';
+                newError.password.valid = false;
+            } else {
+                newError.password.text = '';
+                newError.password.valid = true;
+            }
+        }
+
+        if (e.target.name === 'passwordRepeat') {
+            if (e.target.value !== inputValues.password) {
+                newError.passwordRepeat.text = 'Passwords do not match';
+                newError.passwordRepeat.valid = false;
+            } else {
+                newError.passwordRepeat.text = '';
+                newError.passwordRepeat.valid = true;
+            }
+        }
+
+        setErrors(newError);
+
+        if (newError.password.valid && newError.passwordRepeat.valid) {
+            setValid(true);
+        } else {
+            setValid(false);
+        }
+    };
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        validateInputValues(e);
+
+        setInputValues({
+            ...inputValues,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (valid) {
+            try {
+                const res = await user.register(token!, inputValues.passwordRepeat);
+
+                if(res.ok) {
+                    toast.showToast(ToastType.SUCCESS, 'password set');
+                    setTimeout(async () => {
+                        navigate('/commit');
+                    }, 2000);
+                }
+                else {
+                    toast.showToast(ToastType.ERROR, toast.httpError(res.status, 'Token invalid or expired.'));
+                }
+            }
+            catch(err) {
+                toast.showToast(ToastType.ERROR, 'Connection error. Try again later.');
+            }
+        }
+    };
+
     return (
         <div className="center">
-            <Register />
+            {
+                authenticated === null ? (
+                    <Loader height={32} width={32} borderWidth={5}/>
+                ) : (
+                    <form className="register-form" onSubmit={handleSubmit}>
+                        <div className="input-wrapper">
+                            <div className="input-with-label">
+                                <label htmlFor="password">password:</label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    value={inputValues.password}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <Error text={errors.password.text} />
+                        </div>
+                        <div className="input-wrapper">
+                            <div className="input-with-label">
+                                <label htmlFor="password-repeat">
+                                    password repeat:
+                                </label>
+                                <input
+                                    type="password"
+                                    name="passwordRepeat"
+                                    value={inputValues.passwordRepeat}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <Error text={errors.passwordRepeat.text} />
+                        </div>
+                        <button
+                            type="submit"
+                            className="register-button"
+                            disabled={!valid}
+                        >
+                            set password
+                        </button>
+                    </form>
+                )
+            }
         </div>
     );
 };
