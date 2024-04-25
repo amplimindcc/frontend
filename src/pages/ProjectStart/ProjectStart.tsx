4f3a86f93434e1da9d34fc2e297d58d8bd802921
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2024-04-25 18:29:53 +0200

    add start page structure

---
+++ src/pages/ProjectStart/ProjectStart.tsx
@@ -0,0 +1,25 @@
+import './ProjectStart.css';
+import Button from "../../components/Button/Button";
+import { useNavigate } from 'react-router-dom';
+
+const ProjectStart = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/project/commit');
+    };
+
+    return (
+        <div>
+            <h2>Willkommen zu deine Coding Challenge</h2>
+            <div>
+                Wenn du auf den Startknopf drückst, startet die Challenge und du hast 3 Tage Zeit, um die Aufgabe zu lösen.
+            </div>
+            <div className="start-button">
+                <Button text={"Start"} handleClick={handleClick}/>
+            </div>
+        </div>
+    );
+};
+
+export default ProjectStart;
