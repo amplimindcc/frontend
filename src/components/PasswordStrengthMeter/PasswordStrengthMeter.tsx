fb4437459138f96d82ab4796d1a77dc339dd8ed8
David Linhardt <76444188+MysterionAutotronic@users.noreply.github.com>
2024-04-26 10:18:19 +0200

    create PasswordStrengthMeter

---
+++ src/components/PasswordStrengthMeter/PasswordStrengthMeter.tsx
@@ -0,0 +1,47 @@
+import { create } from 'domain';
+import './PasswordStrengthMeter.css';
+import zxcvbn from 'zxcvbn';
+
+interface PasswordStrengthMeterProps {
+    password: string;
+}
+
+const createPasswordLabel = (score: number): string => {
+    switch (score) {
+        case 0:
+            return 'Weak';
+        case 1:
+            return 'Weak';
+        case 2:
+            return 'Fair';
+        case 3:
+            return 'Good';
+        case 4:
+            return 'Strong';
+        default:
+            return 'Weak';
+    }
+};
+
+const PasswordStrengthMeter: React.FC<PasswordStrengthMeterProps> = ({ password }) => {
+
+    const passwordStrength = zxcvbn(password).score;
+
+    return (
+        <div className='password-strength-meter'>
+            <progress
+                className="password-strength-meter-progress"
+                value={passwordStrength}
+                max="4"
+            />
+            <div>
+                <strong>
+                    {"Password strength: "}
+                </strong>
+                {createPasswordLabel(passwordStrength)}
+            </div>
+        </div>
+    );
+};
+
+export default PasswordStrengthMeter;
