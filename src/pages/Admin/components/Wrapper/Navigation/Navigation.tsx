b1f04ab9274736c7152d8d98e4e247aa49a54881
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2024-04-28 14:46:56 +0200

    improve css

--- src/pages/Admin/components/Wrapper/Navigation/Navigation.tsx
+++ src/pages/Admin/components/Wrapper/Navigation/Navigation.tsx
@@ -35,52 +35,55 @@ export default function Navigation() {
     }, []);
 
     return (
-        <div className="nav-links">
-            <NavLink
-                className={({ isActive }) =>
-                    ['nav-link', isActive ? 'active' : null]
-                        .filter(Boolean)
-                        .join(' ')
-                }
-                end // <-- prevents matching on sub-routes, similar to exact
-                to="/admin/user-management"
-            >
-                Users
-            </NavLink>
-            <NavLink
-                className={({ isActive }) =>
-                    ['nav-link', isActive ? 'active' : null]
-                        .filter(Boolean)
-                        .join(' ')
-                }
-                end // <-- prevents matching on sub-routes, similar to exact
-                to="/admin/submissions-management"
-            >
-                Submissions
-            </NavLink>
-            <NavLink
-                className={({ isActive }) =>
-                    ['nav-link', isActive ? 'active' : null]
-                        .filter(Boolean)
-                        .join(' ')
-                }
-                end // <-- prevents matching on sub-routes, similar to exact
-                to="/admin/exercises-management"
-            >
-                Exercises
-            </NavLink>
-            <div className='nav-spacer'/>
-            <NavLink
-                className={({ isActive }) =>
-                    ['nav-link', isActive ? 'active' : null]
-                        .filter(Boolean)
-                        .join(' ')
-                }
-                end // <-- prevents matching on sub-routes, similar to exact
-                to="/logout"
-            >
-                {username}
-            </NavLink>
+        <div className="nav-bar">
+            <div className="nav-links">
+                <NavLink
+                    className={({ isActive }) =>
+                        ['nav-link', isActive ? 'active' : null]
+                            .filter(Boolean)
+                            .join(' ')
+                    }
+                    end // <-- prevents matching on sub-routes, similar to exact
+                    to="/admin/user-management"
+                >
+                    Users
+                </NavLink>
+                <NavLink
+                    className={({ isActive }) =>
+                        ['nav-link', isActive ? 'active' : null]
+                            .filter(Boolean)
+                            .join(' ')
+                    }
+                    end // <-- prevents matching on sub-routes, similar to exact
+                    to="/admin/submissions-management"
+                >
+                    Submissions
+                </NavLink>
+                <NavLink
+                    className={({ isActive }) =>
+                        ['nav-link', isActive ? 'active' : null]
+                            .filter(Boolean)
+                            .join(' ')
+                    }
+                    end // <-- prevents matching on sub-routes, similar to exact
+                    to="/admin/exercises-management"
+                >
+                    Exercises
+                </NavLink>
+            </div>
+            <div className="nav-bar-username">
+                <NavLink
+                    className={({ isActive }) =>
+                        ['nav-link', isActive ? 'active' : null]
+                            .filter(Boolean)
+                            .join(' ')
+                    }
+                    end // <-- prevents matching on sub-routes, similar to exact
+                    to="/logout"
+                >
+                    {username}
+                </NavLink>
+            </div>
         </div>
     );
 }
