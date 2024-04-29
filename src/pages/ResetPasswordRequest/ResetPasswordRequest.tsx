8cfee8f7706cdd6c0be523107becbcc2287f4056
David Linhardt <divad2012@web.de>
2024-04-29 17:39:18 +0200

    remove unused import

--- src/pages/ResetPasswordRequest/ResetPasswordRequest.tsx
+++ src/pages/ResetPasswordRequest/ResetPasswordRequest.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Link, useNavigate, useParams } from "react-router-dom";
 import user from '../../services/user';
 import './ResetPasswordRequest.css';
