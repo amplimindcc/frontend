f62593bf8e34a41693babac9d156df3c1323dccb
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2024-05-02 11:29:05 +0200

    add request for project fetch of single user

--- src/services/project.ts
+++ src/services/project.ts
@@ -1,23 +1,24 @@
 const baseURL = 'http://localhost:8080/v1';
 
 /**
- * 
+ * Fetches project for an authenticated user
+ * @async
  * @returns {Response}
  */
-const getStatus = async () => {
-    const url = `${baseURL}/submission/active`;
+const getSingleUserProject = async () => {
+    const url = `${baseURL}/project/fetch`;
 
     const res = await fetch(url, {
         method: 'GET',
         credentials: 'include',
         headers: {
             'Content-Type': 'application/json',
-        }
+        },
     });
 
     return res;
 }
 
 export default {
-    getStatus
+    getSingleUserProject
 }
