1978cb67ccb21c863de2f3c6c01ba59633f4ad0f
David Linhardt <76444188+MysterionAutotronic@users.noreply.github.com>
2024-05-02 19:15:27 +0200

    further improvements

--- src/components/ContentWrapper/Footer/Footer.tsx
+++ src/components/ContentWrapper/Footer/Footer.tsx
@@ -3,12 +3,26 @@ import './Footer.css';
 export default function Footer() {
     return (
         <div className="footer background-dark-blue">
-            <a className="footer-link" href="https://amplimind.io/impressum/">Impressum</a>
-            <div className="amplimind">
-                <a className="footer-link" href="https://amplimind.io/">amplimind</a>
-            </div>
-            <div className='github'>
-                <a className="footer-link" href="https://github.com/amplimindcc">GitHub</a>
+            <div className="footer-link-container">
+                <a
+                    className="footer-link"
+                    href="https://amplimind.io/impressum/"
+                >
+                    Impressum
+                </a>
+
+                <a
+                    className="footer-link footer-link-middle"
+                    href="https://amplimind.io/"
+                >
+                    amplimind
+                </a>
+                <a
+                    className="footer-link"
+                    href="https://github.com/amplimindcc"
+                >
+                    GitHub
+                </a>
             </div>
             <p>License Text</p>
         </div>
