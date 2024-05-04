6ee5b995fa6dc556ac15583e1bd330a9905f7941
Matthias Roy <impykid@proton.me>
2024-05-04 08:37:02 +0200

    adding submission service

--- src/services/user.ts
+++ src/services/user.ts
@@ -307,6 +307,41 @@ const resendInvite = async (email: string, isAdmin: boolean) => {
     return res;
 };
 
+/**
+ * Add service : add a new user
+ * http://localhost:8080/swagger-ui/index.html#/admin-controller/createInvite
+ * @async
+ * @param {string} language
+ * @param {string} version
+ * @param {string} filePath
+ * @param {string} [optionalChat]
+ * @returns {Response} HTTP response
+ * @throws {any} connection error
+ */
+const sendSubmission = async (language: string, version: string, filePath: string, optionalChat?: string,): Promise<Response> => {
+    const url = `${baseURL}/submission/submit`;
+
+    const description = optionalChat === undefined ? "" : optionalChat;
+
+    const submission = {
+        description,
+        language,
+        version,
+        filePath
+    }
+
+    const res = await fetch(url, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(submission),
+    });
+
+    return res;
+}
+
 export default {
     login,
     register,
@@ -320,5 +355,6 @@ export default {
     requestPasswordChange,
     changePassword,
     logout,
-    resendInvite
+    resendInvite,
+    sendSubmission
 };
