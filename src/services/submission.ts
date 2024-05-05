748eeb07aef6f4751b76be22698ef94bcaa8d17e
Matthias Roy <impykid@proton.me>
2024-05-05 10:37:41 +0200

    moved submission service

--- src/services/submission.ts
+++ src/services/submission.ts
@@ -1,7 +1,7 @@
 const baseURL = 'http://localhost:8080/v1';
 
 /**
- * 
+ *
  * @returns {Response}
  */
 const getStatus = async () => {
@@ -18,6 +18,42 @@ const getStatus = async () => {
     return res;
 }
 
+/**
+ * Add service : add a new user
+ * http://localhost:8080/swagger-ui/index.html#/admin-controller/createInvite
+ * @async
+ * @param {string} language
+ * @param {string} version
+ * @param {string} filePath
+ * @param {string} [optionalChat]
+ * @returns {Response} HTTP response
+ * @throws {any} connection error
+ */
+const sendSubmission = async (language: string, version: string, filePath: string, optionalChat?: string,): Promise<Response> => {
+    const url = `${baseURL}/submission/submit`;
+
+    const description = optionalChat === undefined ? "" : optionalChat;
+
+    const submission = {
+        description,
+        language,
+        version,
+        filePath
+    }
+
+    const res = await fetch(url, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(submission),
+    });
+
+    return res;
+}
+
 export default {
-    getStatus
+    getStatus,
+    sendSubmission
 }
