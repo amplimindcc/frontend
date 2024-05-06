57292c6aa21d61773c21ccd3f453600221adaedc
samuelhertrich <samuhertrich198@gmail.com>
2024-05-06 18:26:33 +0200

    Add backend connection

--- src/pages/ProjectState/ProjectState.tsx
+++ src/pages/ProjectState/ProjectState.tsx
@@ -1,8 +1,44 @@
+import { useEffect, useState } from 'react';
 import './ProjectState.css';
+import serviceHelper from '../../services/serviceHelper';
+import { useNavigate } from 'react-router-dom';
 
 export default function ProjectState() {
+    const navigate = useNavigate();
+    const [projectReviewed, setProjectReviewed] = useState<Boolean | null>(
+        null
+    );
+
+    useEffect(() => {
+        const getSubmissionState = async () => {
+            const res = await serviceHelper.getSubmissionStatus();
+
+            if (res !== null) {
+                console.log(res.submissionStates);
+
+                if (res.submissionStates === 'INIT') {
+                    navigate('/project/start');
+                }
+
+                if (res.submissionStates === 'IN_IMPLEMENTATION') {
+                    navigate('/project/commit');
+                }
+
+                if (res.submissionStates === 'SUBMITTED') {
+                    setProjectReviewed(false);
+                }
+
+                if (res.submissionStates === 'REVIEWED') {
+                    setProjectReviewed(true);
+                }
+            }
+        };
+        getSubmissionState();
+    }, []);
+
     const isProjectReviewed = () => {
         // TODO: get project state from backend
+        // console.log('isProjectReviewed');
         return false;
     };
 
