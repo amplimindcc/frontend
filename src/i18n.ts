0ac5ab11bab07fe704b3a185ab14b9cae63ae3ce
Matthias Roy <impykid@proton.me>
2024-05-07 16:46:17 +0200

    change translation file location

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,18 +3,18 @@ import { initReactI18next } from "react-i18next";
 
 // Translation files
     // English
-import enMain from "../public/locales/en/main.json";
-import enUserProject from "../public/locales/en/userProject.json";
-import enLogout from "../public/locales/en/logout.json";
-import enInvite from "../public/locales/en/invite.json";
-import enResetPassword from "../public/locales/en/resetPassword.json";
+import enMain from "./locales/en/main.json";
+import enUserProject from "./locales/en/userProject.json";
+import enLogout from "./locales/en/logout.json";
+import enInvite from "./locales/en/invite.json";
+import enResetPassword from "./locales/en/resetPassword.json";
 
     // Deutsch / German
-import deMain from "../public/locales/de/main.json";
-import deUserProject from "../public/locales/de/userProject.json";
-import deLogout from "../public/locales/de/logout.json";
-import deInvite from "../public/locales/de/invite.json";
-import deResetPassword from "../public/locales/de/resetPassword.json";
+import deMain from "./locales/de/main.json";
+import deUserProject from "./locales/de/userProject.json";
+import deLogout from "./locales/de/logout.json";
+import deInvite from "./locales/de/invite.json";
+import deResetPassword from "./locales/de/resetPassword.json";
 
 const resources = {
     en: {
