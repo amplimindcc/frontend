3871c3f0d602a1299766c5bacb38dd38a3d54e76
David Linhardt <76444188+MysterionAutotronic@users.noreply.github.com>
2024-05-18 15:39:21 +0200

    use status codes enum

--- mocks/api.ts
+++ mocks/api.ts
@@ -1,17 +1,18 @@
 import { http, HttpResponse } from 'msw'
+import { StatusCodes } from 'http-status-codes';
 
 const baseURL = import.meta.env.VITE_API_URL;
 
 export const handlers = [
     http.post(`${baseURL}/v1/auth/login`, () => {
         return new HttpResponse(null, {
-            status: 200
+            status: StatusCodes.OK
         })
     }),
 
     http.get(`${baseURL}/v1/auth/check-login`, () => {
         return new HttpResponse(null, {
-            status: 200
+            status: StatusCodes.OK
         })
     }),
 
@@ -25,19 +26,19 @@ export const handlers = [
 
     http.post(`${baseURL}/logout`, () => {
         return new HttpResponse(null, {
-            status: 403
+            status: StatusCodes.FORBIDDEN
         })
     }),
 
     http.post(`${baseURL}/v1/account/request-password-change/user@web.de`, () => {
         return new HttpResponse(null, {
-            status: 200
+            status: StatusCodes.OK
         })
     }),
 
     http.post(`${baseURL}/v1/account/change-password`, () => {
         return new HttpResponse(null, {
-            status: 200
+            status: StatusCodes.OK
         })
     }),
 ]
