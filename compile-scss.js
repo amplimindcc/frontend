07462f9f8b619c9d60258912924a49a1cce207c0
David Linhardt <76444188+MysterionAutotronic@users.noreply.github.com>
2024-05-23 19:48:00 +0200

    add scss compilation script to pipeline

---
+++ compile-scss.js
@@ -0,0 +1,44 @@
+import { exec } from 'child_process';
+import { fs } from 'fs';
+import { path } from 'path';
+
+/**
+ * Compile all SCSS files in their respective directories
+ * @author David Linhardt
+ *
+ * @param {string} dir
+ * @returns {void}
+ */
+function compileScssFile(dir) {
+    fs.readdir(dir, (err, files) => {
+        if (err) throw err;
+        files.forEach((file) => {
+            const filePath = path.join(dir, file);
+            const fileStat = fs.statSync(filePath);
+            if (fileStat.isDirectory()) {
+                compileScssFile(filePath);
+            } else if (path.extname(file) === '.scss') {
+                const cssFilePath = filePath.replace('.scss', '.css');
+                exec(
+                    `sass ${filePath} ${cssFilePath}`,
+                    (err, stdout, stderr) => {
+                        if (err) {
+                            console.error(
+                                `Error compiling ${filePath}:`,
+                                stderr
+                            );
+                            throw err;
+                        } else {
+                            console.log(
+                                `Compiled ${filePath} to ${cssFilePath}`
+                            );
+                        }
+                        console.log(stdout);
+                    }
+                );
+            }
+        });
+    });
+}
+
+compileScssFile(path.join(__dirname, 'src'));
