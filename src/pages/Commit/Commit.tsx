e61d9b7f1d396d2de43b56ce9192cfa03555e100
Matthias Roy <impykid@proton.me>
2024-05-30 14:52:01 +0200

    adding auto line break to description and add max width

--- src/pages/Commit/Commit.tsx
+++ src/pages/Commit/Commit.tsx
@@ -355,7 +355,7 @@ const Commit = () => {
                         <p>{introText}</p>
                         <br />
                         <h3>{t('exerciseHeader')}</h3>
-                        <p>{exerciseText}</p>
+                        <p className='description'>{exerciseText}</p>
                         <br />
                         <form onSubmit={handleSubmit} data-testid="commit-form">
                             <div className="oneLine">
