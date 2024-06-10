fbff616749b906f7d88092f4438001722f0ab1ee
David Linhardt <76444188+MysterionAutotronic@users.noreply.github.com>
2024-06-10 11:51:53 +0200

    add jsdoc

--- src/components/ContentWrapper/PrivacyPolicy/PrivacyPolicy.tsx
+++ src/components/ContentWrapper/PrivacyPolicy/PrivacyPolicy.tsx
@@ -1,8 +1,23 @@
 import './PrivacyPolicy.css';
 import { useTranslation } from 'react-i18next';
 
+/**
+ * Privacy Policy component to display the privacy policy of the application.
+ * @author David Linhardt
+ *
+ * @export
+ * @returns {React.ReactNode}
+ */
 export default function PrivacyPolicy() {
+    // Context
+    /**
+     * i18next Context
+     * @author David Linhardt
+     *
+     * @type {TFunction<[string, string], undefined>}
+     */
     const { t } = useTranslation('privacyPolicy');
+
     return (
         <div className="privacy-policy-container">
             <h1>{t('privacyPolicy')}</h1>
