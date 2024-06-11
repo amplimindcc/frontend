237ed5ff722e67a684172b6067fc71b50f62f8df
David Linhardt <divad2012@web.de>
2024-06-12 00:03:02 +0200

    add jsdoc

--- src/pages/Admin/components/Submissions/Submissions.tsx
+++ src/pages/Admin/components/Submissions/Submissions.tsx
@@ -73,6 +73,14 @@ export default function Submissions() {
     const [rowData, setRowData] = useState<UserSubmissionTableElement[]>([]);
 
     useEffect(() => {
+        /**
+         * connect SSE to the backend to get the submission status changes in real time and update the table accordingly
+         * @author Timo Hauser
+         * @author David Linhardt
+         *
+         * @async
+         * @returns {void}
+         */
         const connect = async () => {
             try {
                 const res = await submission.heartbeat();
